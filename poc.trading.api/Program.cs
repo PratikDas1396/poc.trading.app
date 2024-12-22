dd29af500396f059b990749ec8d43330f3f123a8
Pratik Das <pratikdas1396@gmail.com>
2024-12-22 13:03:05 +0530

    Added swagger for all env

--- poc.trading.api/Program.cs
+++ poc.trading.api/Program.cs
@@ -36,11 +36,14 @@ builder.Services.AddHealthChecks();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHsts();
 app.UseHttpsRedirection();
