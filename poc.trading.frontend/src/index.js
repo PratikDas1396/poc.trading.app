1c00bacc7169d7348b58eb1c8b22bbbf99e80e32
Pratik Das <pratikdas1396@gmail.com>
2024-12-29 15:55:25 +0530

    updated ui route

--- poc.trading.frontend/src/index.js
+++ poc.trading.frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter } from "react-router-dom";
 import StockList from "./pages/StockList/StockList";
 import WatchList from "./pages/WatchList/WatchList";
 import Login from "./pages/Login/Login";
@@ -11,7 +12,7 @@ import { StockContext, StockContextProvider } from "./context/stockContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route
@@ -32,6 +33,6 @@ root.render(
         />
         <Route path="/" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
